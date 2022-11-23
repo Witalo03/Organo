08b1b4319b71c11d8640cea0f2b0fe89f49b3060
Witalo03 <dantaswitalo@gmail.com>
2022-11-23 10:13:01 -0300

    Commit Organo

---
+++ src/componentes/Time/index.js
@@ -0,0 +1,19 @@
+import Colaborador from '../Calaborador'
+import './Time.css'
+
+const Time = (props) => {
+    const bg = { backgroundColor: props.corSecundaria }
+    const bc = { borderColor: props.corPrimaria }
+
+    return (
+        props.colaboradores.length > 0 ? <section className='time' style={bg}>
+            <h3 style={bc}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imgem={colaborador.imgem} />)}
+            </div>
+        </section>
+            : ''
+    )
+}
+
+export default Time
